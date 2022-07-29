f2197fd46bdd875d5c4770453ff78300c1ee06b4
izisopou <54803536+izisopou@users.noreply.github.com>
2022-07-29 17:43:17 +0300

    Update DijetTreeProducer.cc

--- plugins/DijetTreeProducer.cc
+++ plugins/DijetTreeProducer.cc
@@ -782,7 +782,7 @@ void DijetTreeProducer::analyze(edm::Event const& iEvent, edm::EventSetup const&
 
     // https://twiki.cern.ch/twiki/bin/viewauth/CMS/JetID
     int idL = (nhf<0.99 && nemf<0.99 && NumConst>1 && muf < 0.8) && ((fabs(eta) <= 2.4 && chf>0 && chMult>0 && cemf<0.99) || fabs(eta)>2.4);
-    int idT = (nhf<0.90 && nemf<0.90 && NumConst>1 && muf<0.8) && ((fabs(eta)<=2.4 && chf>0 && chMult>0 && cemf<0.90) || fabs(eta)>2.4);
+    int idT = (nhf<0.90 && nemf<0.90 && NumConst>1 && muf<0.8) && ((fabs(eta)<=2.6 && chf>0 && chMult>0 && cemf<0.80) || fabs(eta)>2.6);
 
        
       
@@ -916,7 +916,7 @@ void DijetTreeProducer::analyze(edm::Event const& iEvent, edm::EventSetup const&
 
     // https://twiki.cern.ch/twiki/bin/viewauth/CMS/JetID
     int idL = (nhf<0.99 && nemf<0.99 && NumConst>1 && muf < 0.8) && ((fabs(eta) <= 2.4 && chf>0 && chMult>0 && cemf<0.99) || fabs(eta)>2.4);
-    int idT = (nhf<0.90 && nemf<0.90 && NumConst>1 && muf<0.8) && ((fabs(eta)<=2.4 && chf>0 && chMult>0 && cemf<0.90) || fabs(eta)>2.4);
+    int idT = (nhf<0.90 && nemf<0.90 && NumConst>1 && muf<0.8) && ((fabs(eta)<=2.6 && chf>0 && chMult>0 && cemf<0.80) || fabs(eta)>2.6);
 
       
     if (pt > ptMinAK8_) {
